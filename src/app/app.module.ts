40d49fd7154223454f613ca6583432a502977c06
John Bosco Prasanth <johnbosco.george@twilightsoftwares.com>
2024-07-08 19:49:50 +0530

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { ButtonComponent } from './button/button.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    ButtonComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule
